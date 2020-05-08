b731e804e4ae94fe7348270ae24206f6028b77a5
chuzksy-codeactive <chuzksy@gmail.com>
2020-05-08 18:03:57 +0100

    Commit changes for Presentation Course Controller, Views, Styling

---
+++ CleanArchitecture.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService ?? throw new ArgumentNullException(nameof(courseService));
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = _courseService.GetCourses(); 
+            return View(model);
+        }
+    }
+}
